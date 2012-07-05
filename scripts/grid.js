b7685b53a24af9529780e92110f0a4a759e524e3
Martin Proud <mproud@gmail.com>
2012-07-05 03:46:11 -0500

    One more hover hex instance fixed
    One more hover hex instance fixed.

--- scripts/grid.js
+++ scripts/grid.js
@@ -31,8 +31,8 @@ function preload(arrayOfImages) {
       images[key].src = dataDirectory + 'core/images/terrain/' + value['symbol'];
     }
   });
-  images['hover-hex-top'] = new Image(); images['hover-hex-top'].src = dataDirectory + 'core/images/misc/hover-hex-top.png';
-  images['hover-hex-bottom'] = new Image(); images['hover-hex-bottom'].src = dataDirectory + 'core/images/misc/hover-hex-bottom.png';
+  images['hover-hex-top'] = new Image(); images['hover-hex-top'].src = dataDirectory + '../images/misc/hover-hex-top.png';
+  images['hover-hex-bottom'] = new Image(); images['hover-hex-bottom'].src = dataDirectory + '../images/misc/hover-hex-bottom.png';
   images['hexgrid'] = new Image(); images['hexgrid'].src = 'ui/hexgrid.png';
 }
 
